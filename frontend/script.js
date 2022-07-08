26e04bf3c1954327bf4965c21628487c9ca233ed
ItsRayRay <r.boerema21@gmail.com>
2022-07-08 13:27:50 +0200

    render todo and delete button

--- frontend/script.js
+++ frontend/script.js
@@ -1,23 +1,41 @@
 // light dark mode
 
 function startUp (){
-  const projects = JSON.parse(localStorage.getItem("projects")) || [];
-  projects.forEach(project => {
-    const projectElement = document.createElement("div");
-    projectElement.classList.add("project");
-    projectElement.id = project.id;
-    projectElement.innerHTML = `<li id="modalBtn" class="nav-link">
-<a onclick = "renderTodoList(${project.id})" class="projectTitle" href="#">
-<i class='bx bx-list-check icon'></i>
-    <span id="${project.title}"  class="text nav-text">${project.title}</span>
-</a>
-<i onclick="deleteProject()" class='bx bx-trash icon trashicon' ></i>
-</li>
-`
-    document.querySelector(".menu-links").appendChild(projectElement);
+// loop trough localstorage and get all items
+
+for (let i = 0; i < localStorage.length; i++) {
+  const key = localStorage.key(i);
+  const value = localStorage.getItem(key);
+  const projects = JSON.parse(localStorage.getItem(key)) || [];
+  
+ 
+  console.log(projects[0].title)
+
+  const projectElement = document.createElement("div");
+  projectElement.className = "project";
+  projectElement.id = key;
+  projectElement.innerHTML =`<li id="modalBtn" class="nav-link">
+   <a onclick = "renderTodoList(${key})" class="projectTitle" href="#">
+  <i class='bx bx-list-check icon'></i>
+       <span id="${key}"  class="text nav-text">${projects[0].title}</span>
+  </a>
+   <i onclick="deleteProject(${key})" class='bx bx-trash icon trashicon' ></i>
+  </li>
+  `
+  document.querySelector(".menu-links").appendChild(projectElement);
   }
-  );
+  
+  
 }
+
+
+
+
+
+
+
+
+
 
 
 startUp()
@@ -88,9 +106,9 @@ class Project {
 
 
   storeProject = function() {
-    const projects = JSON.parse(localStorage.getItem("projects")) || [];
+    const projects = JSON.parse(localStorage.getItem(this.id)) || [];
     projects.push(this);
-    localStorage.setItem("projects", JSON.stringify(projects));
+    localStorage.setItem(this.id, JSON.stringify(projects));
 
   }
 }
@@ -104,15 +122,22 @@ class Todo {
   }
 }
 
-function deleteProject() {
-  const projects = JSON.parse(localStorage.getItem("projects")) || [];
-  const id = event.target.dataset.id;
-  const project = projects.find(project => project.id == id);
-  const index = projects.indexOf(project);
-  projects.splice(index, 1);
-  localStorage.setItem("projects", JSON.stringify(projects));
-  location.reload();
-}
+ function deleteProject(ref) {
+  const projects = JSON.parse(localStorage.getItem(ref) || []);
+  projects.forEach(project => {
+    if (project.id == ref) {
+      localStorage.removeItem(ref);
+      location.reload();
+    }
+  }
+  )
+
+  console.log(projects)
+
+
+ }
+
+
 
 const projectTitleInput = document.querySelector(".textinput")
 const projectSubmitBtn = document.querySelector(".submitBtn")
@@ -138,7 +163,7 @@ document.querySelector(".projectTitle").addEventListener("click", function(e) {
 function renderTodoList(ref) {
 
 
- const projects = JSON.parse(localStorage.getItem("projects")) || [];
+ const projects = JSON.parse(localStorage.getItem(ref)) || [];
 
   projects.forEach(project => {
     if (project.id == ref) {
@@ -169,7 +194,7 @@ closeBtn.innerHTML = `<p onclick = "closeTodo()"  >X</p>`;
 toDoTitle.textContent = project.title;
 todoInputFill.placeholder = "Add a task";
 todoButton.textContent = "Add";
-todoButton.innerHTML = `<p onclick = "addTodoToLocalStorage(${project.id}) ">Add</p>`;
+todoButton.innerHTML = `<p onclick = "addTodoToLocalStorage(${ref}) ">Add</p>`;
 homeSection.textContent = "";
 
 homeSection.appendChild(toDo);
@@ -185,11 +210,8 @@ todoForm.appendChild(todoButton);
     }
   }
   )
+  renderTodoToDomElement(ref)
 }
-
-
-
-
 
 
 
@@ -200,32 +222,98 @@ function closeTodo() {
 }
 
 
-
 // 
 
 function addTodoToLocalStorage(ref) {
-  const projectID = document.querySelector(".project")
-  const Projects = JSON.parse(localStorage.getItem("projects")) || [];
+  const Projects = JSON.parse(localStorage.getItem(ref)) || [];
   const todoInputValue = document.querySelector(".todo-input").value;
-  console.log(todoInputValue);
-  const todo = new Todo(todoInputValue, Date.now(), projectID.id);
-  console.log(ref)
-  
+  const todo = new Todo(todoInputValue, Date.now(), ref, false);
+
+    // push todoInputValue to localstorage match it with key of project id
   Projects.forEach(project => {
-    if (project.id == todo.projectId) {
+    if (project.id == ref) {
       project.todo.push(todo);
+      localStorage.setItem(ref, JSON.stringify(Projects));
+     window.location.reload();
+     renderTodoToDomElement(ref);
     }
-  }
-  );
-  localStorage.setItem("projects", JSON.stringify(Projects));
+  } 
+  )
+
 }
 
 
 
-  function renderTodoToDomElement() {
-    const projects = JSON.parse(localStorage.getItem("projects")) || [];
+
+
+
+  function renderTodoToDomElement(ref) {
+
+
+    const todoLabel = document.createElement("label");
+    const todoInput = document.createElement("input");
+    const todoSpanFirst = document.createElement("span");
+    const todoSpanSecond = document.createElement("span");
+    const trash = document.createElement("i");
+    trash.className = "bx bx-trash icon iconTrash";
     
-    console.log(projects[0].todo)
+
+    todoLabel.className = "tasks-list-item";
+    todoInput.className = "tasks-list-cb";
+    todoInput.type = "form-check-input";
+    todoInput.name = "tasks";
+
+
+    // create a new task
+    todoSpanFirst.className = "tasks-list-mark";
+    todoSpanSecond.className = "tasks-list-desc";
+
+
+    const Projects = JSON.parse(localStorage.getItem(ref)) || [];
+  
+    for (let i = 0; i < Projects[0].todo.length; i++) {
+      console.log(Projects[0].todo[i])
+      const todo = document.createElement("div");
+      todo.className = "todo-item";
+      todo.id = Projects[0].todo[i];
+      todo.innerHTML += `<label class="tasks-list-item">
+      <input class="tasks-list-cb" type="checkbox" name="tasks">
+      <span class="tasks-list-mark">
+        <i onclick="setCompletedToTrue(${ref})"class="bx bx-check-circle icon iconCheck"></i>
+      </span>
+      <span class="tasks-list-desc">${Projects[0].todo[i].title}</span>
+      <span class="tasks-list-action">
+        <i  onclick="removeToDoFromLocalStorage(${ref} ,${Projects[0].todo[i].id})" class="bx bx-trash icon iconTrash"></i>
+      </span>
+    </label>`;
+      document.querySelector(".todo-body").appendChild(todo);
+
+      }
+
   }
 
-  renderTodoToDomElement();
+
+
+
+
+  function removeToDoFromLocalStorage(ref, id) {
+  
+    const Projects = JSON.parse(localStorage.getItem(ref)) || [];
+    console.log(id)
+
+    
+     if (confirm("Are you sure you want to delete this task?") || Projects[0].todo.length > 0) {
+        Projects[0].todo.forEach(project => {
+          if (project.id == id) {
+            Projects[0].todo.splice(project, 1);
+            localStorage.setItem(ref, JSON.stringify(Projects));
+            window.location.reload();
+          }
+        }
+        )
+      }
+  
+
+  }
+
+
